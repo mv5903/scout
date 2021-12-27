858f007c0632659c27ad877d48242caaa8960727
Matthew Vandenberg <mv5903@gmail.com>
2021-12-27 14:00:42 -0500

    chankge

--- js/gameCreation/gameCreation.js
+++ js/gameCreation/gameCreation.js
@@ -2,11 +2,19 @@ let joinGame = () => {
     let gameCode = document.querySelector('#input-nickname').value
     let username = document.querySelector('#gameCodeInput').value
 
+
+
     console.log('Join Game Pressed')
+    document.querySelector('.game').style.visibility = 'visible';
+    document.querySelector('.welcome').remove()
 }
 
 let createGame = () => {
     let username = document.querySelector('#gameCodeInput').value
     
+
+    
     console.log('Create Game Pressed')
+    document.querySelector('.game').style.visibility = 'visible';
+    document.querySelector('.welcome').remove()
 }
